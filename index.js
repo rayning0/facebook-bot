773c8e2affd6899830c94d5b2694cbdd9b4b2d98
Raymond Gan <rayning@yahoo.com>
2016-04-20 23:46:44 -0700

    More of the funny

--- index.js
+++ index.js
@@ -5,13 +5,16 @@ var bodyParser = require('body-parser')
 var request = require('request')
 var app = express()
 
-var phrases = ['yo momma!', 'you do that in bed.', 'would you like frogs with that?',
+var phrases = ['yo momma!',
+  'you do that in bed.',
+  'would you like frogs with that?',
   'I love you too! Wait...I thought you were someone else.',
   'my dead grandpa just told me he found that joke funny.',
   "I got it from my Daddy, I got it from my Daddy! I feel nice, you look nice. You'll be my curry, I'll be your rice",
   'vass iss your problem? Ze Doktor iss IN!',
   "stop being so negative. I mean positive. I mean indecisive. Oh hell, I'll shut up now.",
-  'do you know any lady bots who like big botties?']
+  'I like big bots and I cannot lie.',
+  "this is a story all about how my life got flipped-turned upside down and I'd like to take a minute, just sit right there, I'll tell you how I became the prince of a town called Bel-Air"]
 
 app.set('port', (process.env.PORT || 5000))
 
